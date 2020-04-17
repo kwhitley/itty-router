155025b64c0dfde63dccd751dea02be2b3d6ed6f
Kevin Whitley <krwhitley@gmail.com>
2020-04-16 20:56:44 -0500

    adds optional param test, example: /foo/:id?

--- src/Router.spec.js
+++ src/Router.spec.js
@@ -9,24 +9,18 @@ describe('Router', () => {
     { path: '/foo/first', callback: jest.fn(extract), method: 'get' },
     { path: '/foo/:id', callback: jest.fn(extract), method: 'get' },
     { path: '/foo', callback: jest.fn(extract), method: 'post' },
+    { path: '/optional/:id?', callback: jest.fn(extract), method: 'get' },
   ]
 
   for (var route of routes) {
     router[route.method](route.path, route.callback)
   }
 
-  it('is exported via { Router } from Router.js', () => {
+  it(`is exported as { Router } from module`, () => {
     expect(typeof Router).toBe('function')
   })
 
   describe('.handle({ method, url })', () => {
-    it('earlier routes that match intercept later routes', () => {
-      const route = routes.find(r => r.path === '/foo/first')
-      router.handle(buildRequest({ path: '/foo/first' }))
-
-      expect(route.callback).toHaveBeenCalled()
-    })
-
     it('returns { path, query } from match', () => {
       const route = routes.find(r => r.path === '/foo/:id')
       router.handle(buildRequest({ path: '/foo/13?foo=bar&cat=dog' }))
@@ -37,11 +31,28 @@ describe('Router', () => {
       })
     })
 
-    it('honors correct method', () => {
+    it('match earliest routes that match', () => {
+      const route = routes.find(r => r.path === '/foo/first')
+      router.handle(buildRequest({ path: '/foo/first' }))
+
+      expect(route.callback).toHaveBeenCalled()
+    })
+
+    it('honors correct method (e.g. GET, POST, etc)', () => {
       const route = routes.find(r => r.path === '/foo' && r.method === 'post')
       router.handle(buildRequest({ method: 'POST', path: '/foo' }))
 
       expect(route.callback).toHaveBeenCalled()
+    })
+
+    it('handles optional params (e.g. /foo/:id?)', () => {
+      const route = routes.find(r => r.path === '/optional/:id?')
+
+      router.handle(buildRequest({ path: '/optional' }))
+      expect(route.callback).toHaveBeenCalled()
+
+      router.handle(buildRequest({ path: '/optional/13' }))
+      expect(route.callback).toHaveBeenCalledTimes(2)
     })
   })
 })
