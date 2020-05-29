1f596976c5b1d5326296e5b7400c43e63d4a1e96
Kevin Whitley <krwhitley@gmail.com>
2020-05-28 23:48:10 -0500

    v1.1 release

--- src/itty-router.js
+++ src/itty-router.js
@@ -1,26 +1,26 @@
-const Router = () =>
+const Router = (options = {}) =>
   new Proxy({}, {
-    get: (obj, prop) => prop === 'handle' 
+    get: (obj, prop) => prop === 'handle'
       ? async (request) => {
-        for ([route, handlers] of obj[(request.method || 'GET').toLowerCase()] || []) {
-          if (match = (url = new URL(request.url)).pathname.match(route)) { // route matched
-            request.params = match.groups
-            request.query = Object.fromEntries(url.searchParams.entries())
+          for ([route, handlers] of obj[(request.method || 'GET').toLowerCase()] || []) {
+            if (match = (url = new URL(request.url)).pathname.match(route)) {
+              request.params = match.groups
+              request.query = Object.fromEntries(url.searchParams.entries())
 
-            for (handler of handlers) {
-              if ((response = await handler(request)) !== undefined) return response
+              for (handler of handlers) {
+                if ((response = await handler(request)) !== undefined) return response
+              }
             }
           }
         }
-      }
-    : (route, ...handlers) =>
-        (obj[prop] = obj[prop] || []).push([
-          `^${route
-            .replace('*', '.*')
-            .replace(/(\/:([^\/\?]+)(\?)?)/gi, '/$3(?<$2>[^/]+)$3')}$`,
-          handlers
-        ]) && obj
-    }
-  )
+      : (route, ...handlers) =>
+          (obj[prop] = obj[prop] || []).push([
+            `^${(options.base || '')+route
+              .replace(/(\/?)\*/g, '($1.*)?')
+              .replace(/(\/:([^\/\?]+)(\?)?)/gi, '/$3(?<$2>[^/]+)$3')
+            }$`,
+            handlers
+          ]) && obj
+  })
 
 module.exports = { Router }
