b7bc86470fd89727abf35f50306baa716fe7bf86
JP Sugarbroad <jpsugar@amazon.com>
2021-05-29 19:58:55 -0700

    Router golfing, with 37% less CPU in handle path.

--- prebuild.js
+++ prebuild.js
@@ -2,18 +2,19 @@ const { readFileSync, writeFileSync } = require('fs-extra')
 
 const base = readFileSync('./src/itty-router.js', { encoding: 'utf-8' })
 const minifiedBase = base
-  .replace(/\bhandlers\b/g, 'hs') // Handler(S)
-  .replace(/\bhandler\b/g, 'h') // Handler
-  .replace(/([^\.])obj\b/g, '$1t') // Target
-  .replace(/([^\.])options\b/g, '$1o') // Options
-  .replace(/([^\.])receiver\b/g, '$1c') // Options
-  .replace(/([^\.])route\b/g, '$1p') // Path
-  .replace(/([^\.])\.routes\b/g, '$1\.r') // routes Queue
-  .replace(/args/g, 'a') // Args
-  .replace(/([^\.])request\b/g, '$1r') // Request
-  .replace(/([^\.])response\b/g, '$1s') // reSponse
+  .replace(/^\s*\/\/.*\n/mg, '') // remove comments
+  .replace(/\bargs\b/g, 'a') // Args
+  .replace(/\bhandlers\b/g, 'H') // (H)andlers
+  .replace(/\broutes([^:])/g, 'r$1') // Routes
+  .replace(/([^\.])handler\b/g, '$1h') // Handler
   .replace(/([^\.])match\b/g, '$1m') // Match
+  .replace(/([^\.])method\b/g, '$1M') // (M)ethod
   .replace(/([^\.])prop\b/g, '$1k') // Key
+  .replace(/([^\.])receiver\b/g, '$1c') // Options
+  .replace(/([^\.])request\b/g, '$1q') // reQuest
+  .replace(/([^\.])response\b/g, '$1s') // reSponse
+  .replace(/([^\.])route\b/g, '$1p') // Path
+  .replace(/([^\.])target\b/g, '$1t') // Target
   .replace(/([^\.])url\b/g, '$1u') // Url
 writeFileSync('./dist/itty-router.js', minifiedBase)
 console.log('minifying variables --> dist/itty-router.js')
