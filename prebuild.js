362cd7bd88097bcb32dc899f61d7f06cda252d70
Kevin Whitley <krwhitley@gmail.com>
2022-03-24 01:17:25 -0500

    get rid of partial minification in prebuild

--- prebuild.js
+++ prebuild.js
@@ -7,24 +7,6 @@ const esm = base
               .replace(/\s*\/\/[^\n]+/g, '')
 writeFileSync('./dist/itty-router.mjs', esm)
 
-// const minifiedBase = base
-//   .replace(/^\s*\/\/.*\n/mg, '') // remove comments
-//   .replace(/\bargs\b/g, 'a') // Args
-//   .replace(/\bhandlers\b/g, 'H') // (H)andlers
-//   // .replace(/\broutes([^:])/g, 'r$1') // Routes
-//   .replace(/([^\.])handler\b/g, '$1h') // Handler
-//   .replace(/([^\.])match\b/g, '$1m') // Match
-//   .replace(/([^\.])method\b/g, '$1M') // (M)ethod
-//   .replace(/([^\.])prop\b/g, '$1k') // Key
-//   .replace(/([^\.])receiver\b/g, '$1c') // Options
-//   .replace(/([^\.])request\b/g, '$1q') // reQuest
-//   .replace(/([^\.])response\b/g, '$1s') // reSponse
-//   .replace(/([^\.])route\b/g, '$1p') // Path
-//   .replace(/([^\.])target\b/g, '$1t') // Target
-//   .replace(/([^\.])url\b/g, '$1u') // Url
-// writeFileSync('./dist/itty-router.js', minifiedBase)
-// console.log('minifying variables --> dist/itty-router.js')
-
 const test = readFileSync('./src/itty-router.spec.js', { encoding: 'utf-8' })
 const minifiedTest = test.replace('itty-router', 'itty-router.min.js')
 writeFileSync('./dist/itty-router.spec.js', minifiedTest)
