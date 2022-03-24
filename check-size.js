0f77357a6df3af452cc9fb1f5ec9a74b85eda84c
Kevin Whitley <krwhitley@gmail.com>
2022-03-24 00:25:57 -0500

    released v2.5.3 - fixed types to reflect handle returning promise, w tests and docs to match... plus deps update

--- check-size.js
+++ check-size.js
@@ -1,7 +1,6 @@
 const { readFileSync } = require('fs-extra')
-const { magenta } = require('chalk')
 const gzipSize = require('gzip-size')
 
 const base = readFileSync('./dist/itty-router.min.js', { encoding: 'utf-8' })
 
-console.log(magenta('gzipped size:'), gzipSize.sync(base), 'bytes')
+console.log('gzipped size:', gzipSize.sync(base), 'bytes')
---
+++ jest.config.js
@@ -0,0 +1,5 @@
+const config = {
+  moduleNameMapper: {
+    'itty-router': './itty-router.js',
+  }
+}
