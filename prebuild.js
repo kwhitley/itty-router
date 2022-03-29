f9ac47a5bc16a2bd7a8661431901d5c22e2d13e1
Kevin Whitley <krwhitley@gmail.com>
2022-03-28 23:44:22 -0500

    fixes #100 - named export incorrect in mjs/ESM version

--- prebuild.js
+++ prebuild.js
@@ -3,6 +3,7 @@ const { readFileSync, writeFileSync } = require('fs-extra')
 console.log('writing esm version.')
 const base = readFileSync('./src/itty-router.js', { encoding: 'utf-8' })
 const esm = base
+              .replace('function Router', 'export function Router')
               .replace('module.exports =', 'export default')
               .replace(/\s*\/\/[^\n]+/g, '')
 writeFileSync('./dist/itty-router.mjs', esm)
