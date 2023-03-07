7f7768d65e7ee69696e47bec557bf461cdb06eb6
Kevin Whitley <krwhitley@gmail.com>
2023-03-07 00:56:45 -0600

    released v3.0.11-next.34 - can create StatusError without message body

--- src/extras/StatusError.ts
+++ src/extras/StatusError.ts
@@ -7,12 +7,12 @@ export class StatusError extends Error {
   status: number
   [key: string]: any
 
-  constructor(status: number = 500, body: StatusErrorObject | string = 'Internal Server Error') {
-    if (typeof body === 'string') {
-      super(body)
-    } else {
+  constructor(status: number = 500, body?: StatusErrorObject | string) {
+    if (typeof body === 'object') {
       super(body.error)
       Object.assign(this, body)
+    } else {
+      super(body)
     }
 
     this.status = status
