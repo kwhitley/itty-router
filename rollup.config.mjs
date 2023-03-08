ef105e917d32cc8bed5d10e7ceae7aecd3348387
Kevin Whitley <krwhitley@gmail.com>
2023-03-08 12:35:30 -0600

    released v3.0.11-next.40 - added terser and removed error helpers

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -25,7 +25,7 @@ export default async () => {
             ],
             plugins: [
               typescript(),
-              // terser(),
+              terser(),
               bundleSize(),
             ],
           })
