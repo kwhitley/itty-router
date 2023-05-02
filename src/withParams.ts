4a7e6f3b102a7c38ac2b34f7a736349fb7d98e72
Kevin Whitley <krwhitley@gmail.com>
2023-05-02 01:25:36 -0500

    released v4.0.0-next.47 - fixed withParams and downstream withContent collision

--- src/withParams.ts
+++ src/withParams.ts
@@ -3,7 +3,8 @@ import { IRequest } from './Router'
 export const withParams = (request: IRequest): void => {
   request.proxy = new Proxy(request.proxy || request, {
     get: (obj, prop) => {
-      if (obj[prop] !== undefined) return obj[prop]
+      let p
+      if ((p = obj[prop]) !== undefined) return p.bind?.(request) || p
 
       return obj?.params?.[prop]
     }
