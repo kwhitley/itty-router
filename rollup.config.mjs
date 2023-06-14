6bfb32cab209c8a790f12dc49a5b7d64fd9c18a8
Kevin Whitley <krwhitley@gmail.com>
2023-06-14 17:29:21 -0500

    released v4.0.10-next.1 - included types in exports

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,25 +2,39 @@ import terser from '@rollup/plugin-terser'
 import typescript from '@rollup/plugin-typescript'
 import { globby } from 'globby'
 import bundleSize from 'rollup-plugin-bundle-size'
+import copy from 'rollup-plugin-copy'
 
 export default async () => {
-  const files = await globby('./src/*.ts', {
+  const files = (await globby('./src/*.ts', {
     ignore: ['**/*.spec.ts', 'example'],
-  })
+  })).map(path => ({
+    path,
+    shortPath: path.replace(/(\/src)|(\.ts)/g, '').replace('./index', '.'),
+    esm: path.replace('/src/', '/dist/').replace('.ts', '.js'),
+    cjs: path.replace('/src/', '/dist/').replace('.ts', '.cjs.js'),
+    types: path.replace('/src/', '/dist/').replace('.ts', '.d.ts'),
+  }))
 
-  console.log({ files })
+  console.log(files.map(f => f.path))
 
-  return files.map((path) => ({
-    input: path,
+  // const exportInjection = files.map(({ shortPath, esm, cjs, types }) => `
+  //   "${shortPath}": {
+  //     "import": "${esm}",
+  //     "require": "${cjs}",
+  //     "types": "${types}",
+  //   }`).join(',\n')
+
+  return files.map(file => ({
+    input: file.path,
     output: [
       {
         format: 'esm',
-        file: path.replace('/src/', '/dist/').replace('.ts', '.js'),
+        file: file.esm,
         // sourcemap: true,
       },
       {
         format: 'cjs',
-        file: path.replace('/src/', '/dist/').replace('.ts', '.cjs.js'),
+        file: file.cjs,
         // sourcemap: true,
       },
     ],
@@ -28,6 +42,14 @@ export default async () => {
       typescript({ sourceMap: false }),
       terser(),
       bundleSize(),
+      copy({
+        targets: [
+          {
+            src: ['LICENSE'],
+            dest: 'dist',
+          },
+        ],
+      }),
     ],
   }))
 }
