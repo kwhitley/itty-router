fe34a340a048ae4d286d766e533a8ba4f9387c4e
Kevin Whitley <krwhitley@gmail.com>
2023-06-14 16:51:18 -0500

    tempting fate with an export shakeup

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,7 +2,6 @@ import terser from '@rollup/plugin-terser'
 import typescript from '@rollup/plugin-typescript'
 import { globby } from 'globby'
 import bundleSize from 'rollup-plugin-bundle-size'
-import copy from 'rollup-plugin-copy'
 
 export default async () => {
   const files = await globby('./src/*.ts', {
@@ -21,7 +20,7 @@ export default async () => {
       },
       {
         format: 'cjs',
-        file: path.replace('/src/', '/dist/cjs/').replace('.ts', '.js'),
+        file: path.replace('/src/', '/dist/').replace('.ts', '.cjs.js'),
         // sourcemap: true,
       },
     ],
@@ -29,14 +28,6 @@ export default async () => {
       typescript({ sourceMap: false }),
       terser(),
       bundleSize(),
-      copy({
-        targets: [
-          {
-            src: ['CONTRIBUTING.md', 'CODE-OF-CONDUCT.md', 'LICENSE'],
-            dest: 'dist',
-          },
-        ],
-      }),
     ],
   }))
 }
