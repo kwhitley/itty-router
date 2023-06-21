45e8f5a38ac2846aa0a2b6b7ba5b3678fc579fdf
Kevin R. Whitley <krwhitley@gmail.com>
2023-06-21 08:54:00 -0500

    Fixed export map
    * messing with exports again
    
    * released v4.0.11-next.4 - testing full exports again
    
    * released v4.0.11-next.5 - testing without main and module root level attributes
    
    * released v4.0.11-next.5 - testing for TS
    
    * released v4.0.11-next.6 - trying on next.6
    
    * final commits
    
    * released v4.0.11-next.7 - releasing next7 for testing

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,36 +3,51 @@ import typescript from '@rollup/plugin-typescript'
 import { globby } from 'globby'
 import bundleSize from 'rollup-plugin-bundle-size'
 import copy from 'rollup-plugin-copy'
+import fs from 'fs-extra'
+
+// scan files to build
+const files = (await globby('./src/*.ts', {
+  ignore: ['**/*.spec.ts', 'example'],
+})).map(path => ({
+  path,
+  shortPath: path.replace(/(\/src)|(\.ts)/g, '').replace('./index', '.'),
+  esm: path.replace('/src/', '/dist/').replace('.ts', '.mjs'),
+  cjs: path.replace('/src/', '/dist/').replace('.ts', '.js'),
+  types: path.replace('/src/', '/dist/').replace('.ts', '.d.ts'),
+})).sort((a, b) => a.shortPath.toLowerCase() < b.shortPath.toLowerCase() ? -1 : 1)
+
+
+// read original package.json
+const pkg = await fs.readJSON('./package.json')
+
+// create updated exports list from build files
+pkg.exports = files.reduce((acc, file) => {
+  acc[file.shortPath] = {
+    import: file.esm.replace('/dist', ''),
+    require: file.cjs.replace('/dist', ''),
+    types: file.types.replace('/dist', ''),
+  }
+
+  return acc
+}, {})
+
+// write updated package.json
+await fs.writeJSON('./package.json', pkg, { spaces: 2 })
 
 export default async () => {
-  const files = (await globby('./src/*.ts', {
-    ignore: ['**/*.spec.ts', 'example'],
-  })).map(path => ({
-    path,
-    shortPath: path.replace(/(\/src)|(\.ts)/g, '').replace('./index', '.'),
-    esm: path.replace('/src/', '/dist/').replace('.ts', '.mjs'),
-    cjs: path.replace('/src/', '/dist/').replace('.ts', '.js'),
-    types: path.replace('/src/', '/dist/').replace('.ts', '.d.ts'),
-  }))
-
   console.log(files.map(f => f.path))
 
   return files.map(file => ({
     input: file.path,
     output: [
       {
-        format: 'es',
+        format: 'esm',
         file: file.esm,
         sourcemap: false,
       },
       {
         format: 'cjs',
         file: file.cjs,
-        sourcemap: false,
-      },
-      {
-        format: 'cjs',
-        file: file.cjs.replace('dist', 'dist/cjs'),
         sourcemap: false,
       },
     ],
