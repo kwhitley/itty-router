dd29652ad3b71f89d81afe56483db96cac10cc70
Kevin Whitley <krwhitley@gmail.com>
2023-06-21 09:10:11 -0500

    added test coverage to confirm second param of status

--- src/status.spec.ts
+++ src/status.spec.ts
@@ -2,11 +2,22 @@ import 'isomorphic-fetch'
 import { describe, expect, it } from 'vitest'
 import { status } from './status'
 
-describe('status(code: number): Response', () => {
+describe('status(code: number, options?: ResponseInit): Response', () => {
   it('creates a no-content Response with the passed code', async () => {
     const response = status(204)
 
     expect(response.status).toBe(204)
     expect(response.body).toBeFalsy()
+  })
+
+  it('can take a ResponseInit options as second param', async () => {
+    const response = status(204, {
+      headers: {
+        'x-custom': 'FOO'
+      }
+    })
+
+    expect(response.status).toBe(204)
+    expect(response.headers.get('x-custom')).toBe('FOO')
   })
 })
