44b2f8fb767ba372320c498f11a1acb5228fa02e
Kevin Whitley <krwhitley@gmail.com>
2023-06-20 22:44:42 -0500

    released v4.0.11-next.4 - testing full exports again

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,18 +3,38 @@ import typescript from '@rollup/plugin-typescript'
 import { globby } from 'globby'
 import bundleSize from 'rollup-plugin-bundle-size'
 import copy from 'rollup-plugin-copy'
+import fs from 'fs-extra'
+
+// scan files to build
+const files = (await globby('./src/*.ts', {
+  ignore: ['**/*.spec.ts', 'example'],
+})).map(path => ({
+  path,
+  shortPath: path.replace(/(\/src)|(\.ts)/g, '').replace('./index', '.'),
+  esm: path.replace('/src/', '/dist/').replace('.ts', '.mjs'),
+  cjs: path.replace('/src/', '/dist/').replace('.ts', '.js'),
+  types: path.replace('/src/', '/dist/').replace('.ts', '.d.ts'),
+})).sort((a, b) => a.shortPath.toLowerCase() < b.shortPath.toLowerCase() ? -1 : 1)
+
+
+// read original package.json
+const pkg = await fs.readJSON('./package.json')
+
+// create updated exports list from build files
+pkg.exports = files.reduce((acc, file) => {
+  acc[file.shortPath] = {
+    import: file.esm,
+    require: file.cjs,
+    types: file.types,
+  }
+
+  return acc
+}, {})
+
+// write updated package.json
+await fs.writeJSON('./package.json', pkg, { spaces: 2 })
 
 export default async () => {
-  const files = (await globby('./src/*.ts', {
-    ignore: ['**/*.spec.ts', 'example'],
-  })).map(path => ({
-    path,
-    shortPath: path.replace(/(\/src)|(\.ts)/g, '').replace('./index', '.'),
-    esm: path.replace('/src/', '/dist/').replace('.ts', '.mjs'),
-    cjs: path.replace('/src/', '/dist/').replace('.ts', '.js'),
-    types: path.replace('/src/', '/dist/').replace('.ts', '.d.ts'),
-  }))
-
   console.log(files.map(f => f.path))
 
   return files.map(file => ({
@@ -28,11 +48,6 @@ export default async () => {
       {
         format: 'cjs',
         file: file.cjs,
-        sourcemap: false,
-      },
-      {
-        format: 'cjs',
-        file: file.cjs.replace('dist', 'dist/cjs'),
         sourcemap: false,
       },
     ],
