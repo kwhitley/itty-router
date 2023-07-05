fd837db34bd2d18acdb08c68dff9fdddfa1189ac
Kevin Whitley <krwhitley@gmail.com>
2023-07-05 09:30:59 -0500

    code cleanup

--- src/Router.ts
+++ src/Router.ts
@@ -81,14 +81,14 @@ export const Router = <
           [
             prop.toUpperCase(),
             RegExp(`^${(path = (base + route)
-              .replace(/\/+(\/|$)/g, '$1'))  // strip double & trailing splash
-              .replace(/(\/?\.?):(\w+)\+/g, '($1(?<$2>*))')                       // greedy params
-              .replace(/(\/?\.?):(\w+)/g, '($1(?<$2>[^$1/]+?))')               // named params and image format
-              .replace(/\./g, '\\.')                                              // dot in path
-              .replace(/(\/?)\*/g, '($1.*)?')                                     // wildcard
+              .replace(/\/+(\/|$)/g, '$1'))                       // strip double & trailing splash
+              .replace(/(\/?\.?):(\w+)\+/g, '($1(?<$2>*))')       // greedy params
+              .replace(/(\/?\.?):(\w+)/g, '($1(?<$2>[^$1/]+?))')  // named params and image format
+              .replace(/\./g, '\\.')                              // dot in path
+              .replace(/(\/?)\*/g, '($1.*)?')                     // wildcard
             }/*$`),
-            handlers,                                                             // embed handlers
-            path,                                                                 // embed clean route path
+            handlers,                                             // embed handlers
+            path,                                                 // embed clean route path
           ]
         ) && receiver
     }),
