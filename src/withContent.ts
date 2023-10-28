34462d8fffb391f6d54dd565887c31569050b830
Alex Rosenfeld <arosenfeld@seismic.com>
2023-10-28 12:01:19 -0400

    Add a HasContent type for withContent usage convenience

--- src/withContent.ts
+++ src/withContent.ts
@@ -1,7 +1,11 @@
-import { IRequest } from './Router'
+import {IRequest, IRequestStrict} from './Router'
 
 // withContent - embeds any request body as request.content
 export const withContent = async (request: IRequest): Promise<void> => {
   if (request.headers.get('content-type')?.includes('json'))
     request.content = await request.json()
 }
+
+export type HasContent<ContentType> = {
+  content: ContentType,
+} & IRequestStrict
