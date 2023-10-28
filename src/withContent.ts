1cf13034076f7407d75219d5ba9392b813a2c25f
Kevin R. Whitley <krwhitley@gmail.com>
2023-10-28 14:11:27 -0500

    Spacing :)

--- src/withContent.ts
+++ src/withContent.ts
@@ -1,11 +1,11 @@
-import {IRequest, IRequestStrict} from './Router'
+import { IRequest, IRequestStrict } from './Router'
+
+export type HasContent<ContentType> = {
+  content: ContentType,
+} & IRequestStrict
 
 // withContent - embeds any request body as request.content
 export const withContent = async (request: IRequest): Promise<void> => {
   if (request.headers.get('content-type')?.includes('json'))
     request.content = await request.json()
 }
-
-export type HasContent<ContentType> = {
-  content: ContentType,
-} & IRequestStrict
