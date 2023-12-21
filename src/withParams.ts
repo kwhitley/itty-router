1f737b3762e0d512ef3849b8ece91ddde46ffc28
Kevin Whitley <krwhitley@gmail.com>
2023-12-20 21:01:42 -0600

    withParams readability

--- src/withParams.ts
+++ src/withParams.ts
@@ -2,6 +2,8 @@ import { IRequest } from './Router'
 
 export const withParams = (request: IRequest): void => {
   request.proxy = new Proxy(request.proxy || request, {
-    get: (obj, prop) => obj[prop] !== undefined ? obj[prop].bind?.(request) || obj[prop] : obj?.params?.[prop]
+    get: (obj, prop) => obj[prop] !== undefined
+                    ? obj[prop].bind?.(request) || obj[prop]
+                    : obj?.params?.[prop]
   })
 }
