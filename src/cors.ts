c126db0702118a6e101174d576309c096d7b509d
Kevin Whitley <krwhitley@gmail.com>
2024-03-25 12:57:07 -0500

    save a couple bytes by using allowMethods default of string

--- src/cors.ts
+++ src/cors.ts
@@ -13,7 +13,7 @@ export const cors = (options: CorsOptions = {}) => {
   const {
     origin = '*',
     credentials = false,
-    allowMethods = ['*'],
+    allowMethods = '*',
     allowHeaders,
     exposeHeaders,
     maxAge,
