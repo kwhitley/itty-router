4be9d8e8f4f99b8133a093f5450bef9955d44298
Kevin Whitley <krwhitley@gmail.com>
2024-03-28 13:23:51 -0500

    example tweak

--- example/bun-router.ts
+++ example/bun-router.ts
@@ -1,4 +1,4 @@
-import { IRequest } from 'IttyRouter'
+import { IRequest, IRequestStrict } from 'IttyRouter'
 import { Router } from '../src/Router'
 import { error } from '../src/error'
 import { json } from '../src/json'
@@ -8,7 +8,7 @@ const logger = (response: Response, request: IRequest) => {
   console.log(response.status, request.url, '@', new Date().toLocaleString())
 }
 
-const router = Router({
+const router = Router<IRequestStrict>({
   port: 3001,
   before: [withParams],
   finally: [json, logger],
@@ -18,7 +18,7 @@ const router = Router({
 router
   .get('/basic', () => new Response('Success!'))
   .get('/text', () => 'Success!')
-  .get('/params/:foo', ({ foo }) => foo)
+  .get<IRequest>('/params/:foo', ({ foo }) => foo)
   .get('/json', () => ({ foo: 'bar' }))
   .get('/throw', a => a.b.c)
   .all('*', () => error(404))
